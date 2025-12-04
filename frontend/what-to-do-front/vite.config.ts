7c60dc2b4a54b6be2774fc9782aa7fefe03745ea
Thiago <thiagogobb@outlook.com>
2025-12-04 10:30:34 -0300

    update

--- frontend/what-to-do-front/vite.config.ts
+++ frontend/what-to-do-front/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', 
+    // 💡 Defina a nova porta aqui
+    port: 5174, 
+    watch: {
+      usePolling: true,
+    }
+  },
+});
