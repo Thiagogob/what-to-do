d9669c44154a159a2b26ba8ee8809bf1e4e5a118
Thiago <thiagogobb@outlook.com>
2025-12-04 21:04:16 -0300

    update

--- frontend/what-to-do-front/src/App.tsx
+++ frontend/what-to-do-front/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-blue-500 text-white p-4 rounded-lg shadow-xl">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn dadasdasdas uytuyt
+        Click on the Vite and React logos to learn dadasdasdas uytuyt guyg
       </p>
     </>
   )
